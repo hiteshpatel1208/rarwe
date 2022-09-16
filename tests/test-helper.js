88ae1b492a6670a817f303919d79a4b4c3b7413b
hiteshpatel1208 <73196976+hiteshpatel1208@users.noreply.github.com>
2022-09-16 11:47:15 -0400

    Initial commit.

---
+++ tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'rarwes/app';
+import config from 'rarwes/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
