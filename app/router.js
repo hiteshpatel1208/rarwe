c8c03434d838eea912a5dcec144cface563bd5b5
Hitesh Patel <hitesh.patel@crowdstrike.com>
2022-09-23 12:14:52 -0400

    Chapter-5 complete

--- app/router.js
+++ app/router.js
@@ -1,12 +1,14 @@
-import EmberRouter from '@ember/routing/router';
-import config from 'rarwes/config/environment';
-
+import EmberRouter from '@ember/routing/router'
+import config from 'rarwe/config/environment'
 export default class Router extends EmberRouter {
-  location = config.locationType;
-  rootURL = config.rootURL;
+  location = config.locationType
+  rootURL = config.rootURL
 }
 
 Router.map(function () {
-  this.route('bands');
-  this.route('songs');
-});
+  this.route('bands', function () {
+    this.route('band', { path: ':id' }, function () {
+      this.route('songs')
+    })
+  })
+})
--- app/routes/bands.js
+++ app/routes/bands.js
@@ -1,20 +1,68 @@
-import Route from '@ember/routing/route';
-import { tracked } from '@glimmer/tracking';
+import Route from '@ember/routing/route'
+import { tracked } from '@glimmer/tracking'
 
 class Band {
-    @tracked name;
+  @tracked name
 
-    constructor(name) {
-        this.name = name;
-    }
+  constructor({ id, name, songs }) {
+    this.id = id
+    this.name = name
+    this.songs = songs
+  }
+}
+
+class Songs {
+  constructor({ title, rating, band }) {
+    this.title = title
+    this.rating = rating
+    this.band = band
+  }
 }
 
 export default class BandsRoute extends Route {
-    model() {
-        return [
-            new Band('Led Zeppelin'),
-            new Band('Pearl Jam'),
-            new Band('Foo Fighters'),
-        ];
-    }
+  model() {
+    const blackDog = new Songs({
+      title: 'Black Dog',
+      band: 'Led Zeppelin',
+      rating: 3,
+    })
+
+    const yellowLedbetter = new Songs({
+      title: 'Yellow Ledbetter',
+      band: 'Pearl Jam',
+      rating: 4,
+    })
+
+    const pretender = new Songs({
+      title: 'The Pretender',
+      band: 'Foo Fighters',
+      rating: 2,
+    })
+
+    const daughter = new Songs({
+      title: 'Daughter',
+      band: 'Pearl Jam',
+      rating: 5,
+    })
+
+    const ledZeppelin = new Band({
+      id: 'led-zeppelin',
+      name: 'Led Zeppelin',
+      songs: [blackDog],
+    })
+
+    const pearlJam = new Band({
+      id: 'pearl-jam',
+      name: 'Pearl Jam',
+      songs: [yellowLedbetter, daughter],
+    })
+
+    const fooFighters = new Band({
+      id: 'foo-fighters',
+      name: 'Foo Fighters',
+      songs: [pretender],
+    })
+
+    return [ledZeppelin, pearlJam, fooFighters]
+  }
 }
