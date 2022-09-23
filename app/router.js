e1c29734a1902ac374622727f054a30d44a6d067
Hitesh Patel <hitesh.patel@crowdstrike.com>
2022-09-23 12:54:16 -0400

    Revert "Chapter-5 complete"
    This reverts commit c8c03434d838eea912a5dcec144cface563bd5b5.

--- app/router.js
+++ app/router.js
@@ -1,14 +1,12 @@
-import EmberRouter from '@ember/routing/router'
-import config from 'rarwe/config/environment'
+import EmberRouter from '@ember/routing/router';
+import config from 'rarwes/config/environment';
+
 export default class Router extends EmberRouter {
-  location = config.locationType
-  rootURL = config.rootURL
+  location = config.locationType;
+  rootURL = config.rootURL;
 }
 
 Router.map(function () {
-  this.route('bands', function () {
-    this.route('band', { path: ':id' }, function () {
-      this.route('songs')
-    })
-  })
-})
+  this.route('bands');
+  this.route('songs');
+});
--- app/routes/bands/band/songs.js
+++
@@ -1,8 +0,0 @@
-import Route from '@ember/routing/route'
-
-export default class BandsBandSongsRoute extends Route {
-  model() {
-    let band = this.modelFor('bands.band')
-    return band.songs
-  }
-}
